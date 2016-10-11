95e9934f743cec42065a1d14fa3c4db304a9160d
DrAlexMV <alexandermarquina@gmail.com>
2016-10-11 01:01:20 -0700

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,14 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const debug = require('debug')('app:webpack:config')
 
+const basePath = path.resolve(__dirname);
 
+console.log(path.join(basePath, "src/main.tsx"));
 module.exports = {
-    entry: "./src/main.tsx",
+    entry: path.join(basePath, "src/main.tsx"),
     output: {
-        filename: "./dist/bundle.js",
+        filename: path.join(basePath, "dist/bundle.js"),
+        path: path.join(basePath, "dist")
     },
     devtool: "source-map",
     resolve: {
@@ -23,7 +26,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: path.join(basePath, 'src/index.html'),
             hash: false,
             filename: 'index.html',
             inject: 'body',
